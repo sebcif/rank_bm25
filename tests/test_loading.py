9c5dd6d616c7df71c7bdfc8903945fd404ce40cc
Sebastian Cifuentes <sebastian.cifuentes@cenia.cl>
2023-10-11 19:41:39 -0300

    Add basic tests to test_loading

--- tests/test_loading.py
+++ tests/test_loading.py
@@ -3,7 +3,7 @@ import os
 myPath = os.path.dirname(os.path.abspath(__file__))
 sys.path.insert(0, myPath + '/../')
 
-from rank_bm25 import BM25Okapi, BM25L, BM25Plus
+from rank_bm25 import BM25Okapi, BM25L, BM25Plus, BM25OkapiWithIDs
 import re
 
 
@@ -20,13 +20,20 @@ algs = [
     BM25Plus(tokenized_corpus)
 ]
 
+corpus_id = [(f"id{i}", text) for i, text in enumerate(corpus)]
+tokenized_corpus_id = [(t[0].split(" "), t[1]) for t in corpus_id]
+
+algs_id = [BM25OkapiWithIDs(tokenized_corpus_id)]
 
 def test_corpus_loading():
     for alg in algs:
         assert alg.corpus_size == 3
         assert alg.avgdl == 5
         assert alg.doc_len == [4, 6, 5]
-
+    for alg in algs_id:
+        assert alg.corpus_size == 3
+        assert alg.avgdl == 5
+        assert list(alg.doc_len.values()) == [4, 6, 5]
 
 def tokenizer(doc):
     return doc.split(" ")
@@ -37,3 +44,7 @@ def test_tokenizer():
     assert bm25.corpus_size == 3
     assert bm25.avgdl == 5
     assert bm25.doc_len == [4, 6, 5]
+    bm25_ids = BM25OkapiWithIDs(corpus_id, tokenizer=tokenizer)
+    assert bm25_ids.corpus_size == 3
+    assert bm25_ids.avgdl == 5
+    assert list(bm25_ids.doc_len.values()) == [4, 6, 5]
